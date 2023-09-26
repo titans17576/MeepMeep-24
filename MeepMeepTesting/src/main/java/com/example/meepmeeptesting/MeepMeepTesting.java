793eca36e89aea1ffa3efed1620f1f392872bcdd
d4rkra1 <noahd5689@gmail.com>
2023-09-25 20:06:47 -0500

    meepmeep setup

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -5,25 +5,34 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
+                        drive.trajectorySequenceBuilder(new Pose2d(60, -60, 0))
                                 .turn(Math.toRadians(90))
-                                .forward(30)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
+                                .forward(120)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
+                                .forward(120)
+                                .turn(Math.toRadians(90))
+                                .forward(120)
                                 .turn(Math.toRadians(90))
                                 .build()
                 );
+        //Image img = null;
+        //try { img = ImageIO.read(new File("CENTERSTAGE_OFFICIAL.png")); }
+        //catch (IOException e) {}
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
