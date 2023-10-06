1070deca3b893de1ef2a945171c1a9ea35da4af8
d4rkra1 <noahd5689@gmail.com>
2023-10-05 20:14:05 -0500

    random auto driving

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -20,7 +20,7 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .setDimensions(12,16)
                 .followTrajectorySequence(drive ->
-                        Noah.path1(drive, new Pose2d(60,60,Math.toRadians(180)))
+                        Noah.blueRight(drive, new Pose2d(60,60,Math.toRadians(180)))
                 );
         Image img = null;
         try { img = ImageIO.read(new File("./MeepMeepTesting/src/main/java/com/example/meepmeeptesting/CENTERSTAGE_OFFICIAL.png")); }
--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Noah.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Noah.java
@@ -5,9 +5,23 @@ import com.noahbres.meepmeep.roadrunner.DriveShim;
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
 
 public class Noah {
-    public static TrajectorySequence path1(DriveShim drive, Pose2d startPose){
-        return drive.trajectorySequenceBuilder(startPose)
-                .turn(Math.toRadians(45))
+    public static TrajectorySequence blueRight(DriveShim drive, Pose2d startPose){
+        return drive.trajectorySequenceBuilder(new Pose2d(-60, -34, 0))
+                //if TeamProp=left
+                    //.lineToSplineHeading(new Pose2d(-35, -34, Math.toRadians(90)))
+                //else if TeamProp=center
+                    //.lineToSplineHeading(new Pose2d(-35, -36, Math.toRadians(0)))
+                //else if TeamProp=right
+                    .lineToSplineHeading(new Pose2d(-35, -36, Math.toRadians(270)))
+
+                .build();
+
+    }
+    public static TrajectorySequence blueLeft(DriveShim drive, Pose2d startPose){
+        return drive.trajectorySequenceBuilder(new Pose2d(-60, -34, 0))
+
+                .lineToSplineHeading(new Pose2d(-50, 34, Math.toRadians(90)))
                 .build();
     }
 }
+
